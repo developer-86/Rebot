f0e0e5f16623b4a558546e1fc4a587c6e6933820
developer-86 <developer86@outlook.com>
2017-12-21 23:54:38 +0300

    Test commit

---
+++ Bot/BotConsts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot
+{
+    /// <summary>
+    /// Consts class
+    /// </summary>
+    public class BotConsts
+    {
+        public const int AppApiID = 176502;
+        public const string AppApiHash = "fc7a77bbc7539a02df29bb3652f5f995";
+
+        public const int BotID = 464216542;
+        public const string BotHash = "AAF25FiE-zNH1w-tdu45BVVKQRx1XGwgRZg";
+
+        public const string Phone = "79262198816";
+    }
+}
---
+++ Bot/BotUser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+
+namespace Bot
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class BotUser
+    {
+        internal BotUser(Bot bot, TLUser tUser)
+        {
+            if (bot == null)
+                throw new ArgumentNullException("bot");
+
+            if (tUser == null)
+                throw new ArgumentNullException("tUser");
+
+            this.Bot = bot;
+            this.TUser = tUser;
+        }
+
+        public Bot Bot { get; private set; }
+
+        public TLUser TUser { get; private set; }
+
+        private bool __init_UserName;
+        private string _UserName;
+        public string UserName
+        {
+            get
+            {
+                if (!__init_UserName)
+                {
+                    _UserName = this.TUser.Username;
+                    __init_UserName = true;
+                }
+                return _UserName;
+            }
+        }
+
+        private bool __init_Id;
+        private int _Id;
+        public int Id
+        {
+            get
+            {
+                if (!__init_Id)
+                {
+                    _Id = this.TUser.Id;
+                    __init_Id = true;
+                }
+                return _Id;
+            }
+        }
+
+        private bool __init_FirstName;
+        private string _FirstName;
+        public string FirstName
+        {
+            get
+            {
+                if (!__init_FirstName)
+                {
+                    _FirstName = this.TUser.FirstName;
+                    __init_FirstName = true;
+                }
+                return _FirstName;
+            }
+        }
+
+        private bool __init_Phone;
+        private string _Phone;
+        public string Phone
+        {
+            get
+            {
+                if (!__init_Phone)
+                {
+                    _Phone = this.TUser.Phone;
+                    __init_Phone = true;
+                }
+                return _Phone;
+            }
+        }
+    }
+}
