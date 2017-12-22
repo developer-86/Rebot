47853ddde4d0e5e9a42345ed20cd1a85a6f31d15
developer-86 <developer86@outlook.com>
2017-12-22 12:20:25 +0300

    Add configurations projects

---
+++ Bot.Configuration.AppConfig/AppConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Configuration.AppConfig
+{
+    public class AppConfiguration : IConfiguration
+    {
+        public string GetParameterStringValue(string parameterName, bool throwIfEmpty = true)
+        {
+            if (string.IsNullOrEmpty(parameterName))
+                throw new ArgumentNullException("parameterName");
+
+            string value = ConfigurationManager.AppSettings[parameterName];
+            if (throwIfEmpty && string.IsNullOrEmpty(value))
+                throw new Exception(string.Format("Параметр конфигурации {0} не задан",
+                    parameterName));
+
+            return value;
+        }
+
+        public int GetParameterIntegerValue(string parameterName, bool throwIfEmpty = true)
+        {
+            if (string.IsNullOrEmpty(parameterName))
+                throw new ArgumentNullException("parameterName");
+
+            string stringValue = this.GetParameterStringValue(parameterName, throwIfEmpty);
+            int value;
+            bool valid = int.TryParse(stringValue, out value);
+            if (!valid && throwIfEmpty)
+                throw new Exception(string.Format("Параметр конфигурации {0} не задан",
+                    parameterName));
+
+            return value;
+        }
+    }
+}
---
+++ Bot.Configuration/IConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Configuration
+{
+    public interface IConfiguration
+    {
+        string GetParameterStringValue(string parameterName, bool throwIfEmpty = true);
+
+        int GetParameterIntegerValue(string parameterName, bool throwIfEmpty = true);
+    }
+}
