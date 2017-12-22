e8b3c1a9964892a0e38cb571be27bad890ed4e31
developer-86 <developer86@outlook.com>
2017-12-22 12:02:32 +0300

    Fix multithread uploading

--- Bot/BotConsts.cs
+++ Bot/BotConsts.cs
@@ -11,12 +11,6 @@ namespace Bot
     /// </summary>
     public class BotConsts
     {
-        public const int AppApiID = 176502;
-        public const string AppApiHash = "fc7a77bbc7539a02df29bb3652f5f995";
-
-        public const int BotID = 464216542;
-        public const string BotHash = "AAF25FiE-zNH1w-tdu45BVVKQRx1XGwgRZg";
-
-        public const string Phone = "79262198816";
+        
     }
 }
